3bb2bba84ba9a8c667b1f02e029720ffc3e1a0c2
lyandrovieira <lyandro.melo@gmail.com>
2023-05-16 02:16:49 -0300

    Início do projeto/Criação classe principal e janela de cadastro.

---
+++ Inventario/src/formularios/Inventario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formularios;
+
+/**
+ *
+ * @author lyand
+ */
+public class Inventario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CadastroBens cadastro = new CadastroBens();
+        cadastro.setVisible(true);
+    }
+    
+}
